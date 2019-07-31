d5c1456da31a4b94e504bedec77f30172b39b348
Annie <anniekao@trentu.ca>
2019-07-31 18:35:19 +0000

    Added message by pressing 't' on keyboard

--- input.js
+++ input.js
@@ -15,22 +15,26 @@ const setupInput = function(conn) {
 const handlerUserInput = key => {
   if (key === "\u0003") {
     process.exit();
-
-    switch (key) {
-    case "w":
-      connection.write("Move: up");
-      break;
-    case "a":
-      connection.write("Move: left");
-      break;
-    case "s":
-      connection.write("Move: down");
-      break;
-    case "d":
-      connection.write("Move: right");
-      break;
-    }   
   }
+
+  switch (key) {
+  case "w":
+    connection.write("Move: up");
+    break;
+  case "a":
+    connection.write("Move: left");
+    break;
+  case "s":
+    connection.write("Move: down");
+    break;
+  case "d":
+    connection.write("Move: right");
+    break;
+  case "t":
+    connection.write("Say: Wooo!", () => {});
+    break;
+  }
+  
 };
 
 module.exports = { setupInput };
